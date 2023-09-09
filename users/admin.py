a944bb2a7db74b8cb1d34b8698ccf490da594c59
Gbenga George <dtechydude@gmail.com>
2023-09-09 23:25:18 +0100

    student search feature

--- users/admin.py
+++ users/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from users.models import Profile
 
+
 class UserProfileAdmin(admin.ModelAdmin):
        
     list_display=('user', 'code', 'user_type')
@@ -10,4 +11,16 @@ admin.site.register(Profile, UserProfileAdmin)
 
 
 # Register your models here.
+
+# # Hide first name and last name from admin panel
+# class UserAdminCustom(UserAdmin):
+
+#     def get_fieldsets(self, request, obj=None):
+#         fieldsets = super().get_fieldsets(request, obj)
+#         new = []
+#         for name, fields_dict in fieldsets:
+#             if fields_dict['fields'] == ('first_name', 'last_name',):
+#                 fields_dict['fields'] = ('email',)
+#             new.append((name, fields_dict))
+#         return new
 
